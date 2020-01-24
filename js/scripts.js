37d061c2cb6504273fe792f6efd3ab587206cc8d
blacchoy@gmail.com <blacchoy@gmail.com>
2020-01-24 11:57:24 -0800

    add working hello to js file

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,20 @@ $(document).ready(function() {
   $("form").submit(function(event){
     event.preventDefault();
 
-    var userAnswer = $("input#kitty").val();
-    $("#output").text(userAnswer);
+    var string = $("input#kitty").val();
+    var userAnswers = string.split('');
+    console.log(userAnswers);
 
-})
-				
+    for (var index =0; index < userAnswers.length; index +=1) {
+      if (userAnswers[index] === '1') {
+          userAnswers.splice(index,1,'hello')
+      } else (userAnswers[index] < '1') 
+          userAnswers.splice(index,1,'kitty')
+    };
+
+      $("#output").text(userAnswers);
+
+
+      });
 });
+				
