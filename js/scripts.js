be229f9a7eb59d6b57af863c605c4354a5dd70c2
blacchoy@gmail.com <blacchoy@gmail.com>
2020-01-24 15:03:17 -0800

    add additional css stylingsuch as gradient background and text formatting

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,22 @@ $(document).ready(function() {
 
     var string = $("input#kitty").val();
     var userAnswers = string.split('');
-    console.log(userAnswers);
 
-    for (var index =0; index < userAnswers.length; index +=1) {
-      if (userAnswers[index] === '1') {
-          userAnswers.splice(index,1,'hello')
-      } else if (userAnswers[index] === '2') 
-          userAnswers.splice(index,2,'kitty')
-    };
+    for (var number =0; number < userAnswers.length; number +=1) {
+      if (userAnswers[number] === '1') {
+          userAnswers.splice(number,1,'hello')
+      } else if (userAnswers[number] === '1') {
+          userAnswers.splice(number,2,'kitty')
+      } else if (userAnswers[number] === '2') {
+          userAnswers.splice(number,2,'kitty')
+      } else if (userAnswers[number] === '3') {
+          userAnswers.splice(number,3,'hello kitty no kno threes')
+          
+    }
+  }
+
 
       $("#output").text(userAnswers);
-
 
       });
 });
