d31acfdafd6d52df0c62cbd2f6ca9f6c9339616f
blacchoy@gmail.com <blacchoy@gmail.com>
2020-01-24 09:00:55 -0800

    add CSS styling with robot image, text and button

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 $(document).ready(function() {
+  $("form").submit(function(event){
+    event.preventDefault();
+
+    var userAnswer = $("input#kitty").val();
+    $("#output").text(userAnswer);
+
+})
+				
+});
