f94a924e35b0bfceb53cd006dfff9cc6ef23ed2a
blacchoy@gmail.com <blacchoy@gmail.com>
2020-01-30 07:22:23 -0800

    separating user and business logic within js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,37 @@
 $(document).ready(function() {
-  $("form").submit(function(event){
+  $("form").submit(function(event) {
     event.preventDefault();
 
-    var string = $("input#kitty").val();
-    var userAnswers = string.split('');
-    var list = userAnswers;
+    var userInput = parseInt($("input#kitty").val());
+    var kittyResponse = robot(userInput);
 
-    for (var number =0; number < userAnswers.length; number +=1) {
-      if (userAnswers[number] === '1') {
-          userAnswers.splice(number,1,'hello')
-      } else if (userAnswers[number] === '1') {
-          userAnswers.splice(number,2,'kitty')
-      } else if (userAnswers[number] === '2') {
-          userAnswers.splice(number,2,'kitty')
-      } else if (userAnswers[number] === '3') {
-          userAnswers.splice(number,3,'hello kitty no kno threes')
-    }
-  }
-
-      $("#output").text(userAnswers);
-
-      });
+    $("#output").text(kittyResponse);
+  });
 });
-				
+
+
+// $(document).ready(function() {
+//   $("form").submit(function(event){
+//     event.preventDefault();
+
+//     var string = $("input#kitty").val();
+//     var userAnswers = string.split('');
+//     var list = userAnswers;
+
+//     for (var number =0; number < userAnswers.length; number +=1) {
+//       if (userAnswers[number] === '1') {
+//           userAnswers.includes(number,1,'hello')
+//       } else if (userAnswers[number] === '1') {
+//           userAnswers.splice(number,2,'kitty')
+//       } else if (userAnswers[number] === '2') {
+//           userAnswers.splice(number,2,'kitty')
+//       } else if (userAnswers[number] === '3') {
+//           userAnswers.splice(number,3,'hello kitty no kno threes')
+//     }
+//   }
+
+//       $("#output").text(userAnswers);
+
+//       });
+// });
+		
